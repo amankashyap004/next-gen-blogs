acbc8357326eec2dadf2f10e1ba6dd136a49d6b4
amankashyap004 <amankashyap004@gmail.com>
2023-07-28 19:00:11 +0530

    footer component

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Footer = () => {
-   return <div>Footer</div>;
+   return (
+      <div className="py-6 px-8 bg-slate-800">
+         <p>@ {new Date().getFullYear()} Next Gen Blogs. All rights reserved.</p>
+      </div>
+   );
 };
 
 export default Footer;
