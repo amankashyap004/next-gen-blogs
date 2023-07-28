b92245e974867e4d297774b1b0e3ca3077f2ab48
amankashyap004 <amankashyap004@gmail.com>
2023-07-28 18:33:37 +0530

    add navbar and footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Navbar from "@/components/navbar/Navbar";
+import Footer from "@/components/footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +15,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
    return (
       <html lang="en">
          <body className={inter.className} suppressHydrationWarning={true}>
+            <Navbar />
             {children}
+            <Footer />
          </body>
       </html>
    );
