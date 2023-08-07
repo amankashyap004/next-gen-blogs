4c1c3bf073dc79efb1df9c7a65d44d73d04e3c7a
aman <amankashyap004@gmail.com>
2023-08-07 19:27:16 +0530

    portfolio category page

--- src/app/portfolio/[category]/page.tsx
+++ src/app/portfolio/[category]/page.tsx
@@ -1,32 +1,53 @@
 import React from "react";
 import Image from "next/image";
 import Button from "@/components/Button";
+import { items } from "./data";
+import { notFound } from "next/navigation";
 
 interface CategoryProps {
-   params: {
-      category: string;
-   };
+  params: {
+    category: any;
+  };
 }
 
+type Category = "applications" | "illustrations" | "websites";
+
+const getData = (cat: Category) => {
+  const data = items[cat];
+
+  if (data) {
+    return data;
+  }
+
+  return notFound();
+};
+
 const Category: React.FC<CategoryProps> = ({ params }) => {
-   return (
-      <div>
-         <h1 className="text-green-500">{params.category}</h1>
-         <section className="flex gap-10 mt-4 mb-8 flex-1 w-3/4">
-            <div className="flex justify-center flex-col flex-1 gap-4">
-               <h1 className="text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
-               <p className="text-base">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo autem aut quasi
-                  culpa omnis doloribus nobis sapiente veritatis sequi velit?
-               </p>
-               <Button text="See More" url="#" />
-            </div>
-            <div className="flex flex-1 h-80 relative">
-               <Image src="/image1.jpg" fill={true} alt="image" className="object-cover" />
-            </div>
-         </section>
-      </div>
-   );
+  const data = getData(params.category);
+
+  return (
+    <div>
+      <h1 className="text-green-500">{params.category}</h1>
+      {data.map((item) => (
+        <section className="flex gap-10 mt-4 mb-8 flex-1 w-3/4" key={item.id}>
+          <div className="flex justify-center flex-col flex-1 gap-4">
+            <h1 className="text-lg">{item.title}</h1>
+            <p className="text-base"> {item.desc}</p>
+            <Button text="See More" url="#" />
+          </div>
+          <div className="flex flex-1 w-full h-80 relative">
+            <Image
+              src={item.image}
+              fill={true}
+              alt="image"
+              className="object-cover"
+              sizes="100vw"
+            />
+          </div>
+        </section>
+      ))}
+    </div>
+  );
 };
 
 export default Category;
